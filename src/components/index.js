39485141b33c6b1383a0e595e87e515fb60e8b28
zd4y <ariasorozco03@gmail.com>
2020-04-30 10:35:11 -0500

    Fix bad import

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,6 @@ import MovieList from './MovieList';
 import MovieListItem from './MovieListItem';
 import Navbar from './Navbar';
 import MovieDetails from './MovieDetails';
-import Error from './';
+import Error from './Error';
 
 export { MovieBrowser, MovieList, Navbar, MovieDetails, MovieListItem, Error };
