1f9c701c1f9e0c8201c0f67c544aa8f2a33f9ad6
zd4y <ariasorozco03@gmail.com>
2020-05-02 21:25:30 -0500

    Improve useQuery hook

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -1,6 +1,11 @@
 import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function useQuery() {
   const location = useLocation();
-  return new URLSearchParams(location.search);
+  const [params, setParams] = useState(new URLSearchParams(location.search));
+  useEffect(() => {
+    setParams(new URLSearchParams(location.search));
+  }, [location]);
+  return params;
 }
